3ec3d63e7d59fb5d01085a96b9cc397d7bf5c156
Viney <vineykhaneja999@gmail.com>
2022-02-10 11:43:55 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlipCoin
+{
+    internal class Combination
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Flip Coin Combination Program");
+            Console.ReadLine();
+        }
+
+    }
+}
