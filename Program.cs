30f63fad10618dfe889ecb6ae18df3fb4f90a020
Viney <vineykhaneja999@gmail.com>
2022-02-10 11:52:33 +0530

    FEATURE ADDED: Displaying HEAD or TAIL

--- Program.cs
+++ Program.cs
@@ -11,6 +11,20 @@ namespace FlipCoin
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Flip Coin Combination Program");
+            //CONSTATNTS
+            const int HEAD = 1;
+
+            Random random = new Random();
+            int flipCoin = random.Next(0,2);
+            if (flipCoin == HEAD)
+            {
+                Console.WriteLine("HEAD");
+            }
+            else
+            {
+                Console.WriteLine("TAIL");
+            }
+
             Console.ReadLine();
         }
 
