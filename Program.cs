c87646d3faa8b6241a9eb2f1dff42fc4a1494943
Viney <vineykhaneja999@gmail.com>
2022-02-10 12:24:22 +0530

    FEATURE ADDED: %age of Singlet Combination

--- Program.cs
+++ Program.cs
@@ -13,17 +13,40 @@ namespace FlipCoin
             Console.WriteLine("Welcome to Flip Coin Combination Program");
             //CONSTATNTS
             const int HEAD = 1;
+            const int FLIPCOINTIMES = 20;
 
-            Random random = new Random();
-            int flipCoin = random.Next(0,2);
-            if (flipCoin == HEAD)
+            //VARIABLES
+            int i1 = 1;
+            int headCounter = 0;
+            int tailCounter = 0;
+            IDictionary<int, string> singletCombination = new Dictionary<int, string>();
+
+            while ( i1 <= FLIPCOINTIMES)
             {
-                Console.WriteLine("HEAD");
+                Random random = new Random();
+                int flipCoin = random.Next(0, 2);
+                if (flipCoin == HEAD)
+                {
+                    //Console.WriteLine("HEAD");
+                    singletCombination[i1] = "H ";
+                    headCounter++;
+                }
+                else
+                {
+                    //Console.WriteLine("TAIL");
+                    singletCombination[i1] = "T ";
+                    tailCounter++;
+                }
+            i1++;
             }
-            else
+            for (int i2 = 1; i2 <= singletCombination.Count; i2++)
             {
-                Console.WriteLine("TAIL");
+                Console.Write(singletCombination[i2]);
             }
+            Console.WriteLine();
+            Console.WriteLine($"Perentage of Head Singlet is {(headCounter * 100) / FLIPCOINTIMES}");
+            Console.WriteLine($"Perentage of Tail Singlet is {(tailCounter * 100) / FLIPCOINTIMES}");
+            
 
             Console.ReadLine();
         }
